0eb90ddcb0b4675938ba66797be42565ff4f2060
Vasiliy Voronko <mail@voronko.dev>
2022-11-28 14:34:48 +0300

    Изменение в иерархии проекта.

---
+++ Assets/Scripts/Controllers/ProjectileController.cs
@@ -0,0 +1,30 @@
+﻿using Components.Abstract;
+using Entities;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class ProjectileController : MonoBehaviour
+    {
+        [SerializeField] private Entity _unit;
+        
+        private ISelfDestroyComponent _selfDestroyComponent;
+        private IMoveComponent _moveComponent;
+
+        private void Awake()
+        {
+            _selfDestroyComponent = _unit.Get<ISelfDestroyComponent>();
+            _moveComponent = _unit.Get<IMoveComponent>();
+        }
+
+        private void Start()
+        {
+            _selfDestroyComponent.StartDestruction();
+        }
+
+        private void Update()
+        {
+            _moveComponent.Move(transform.forward);
+        }
+    }
+}
